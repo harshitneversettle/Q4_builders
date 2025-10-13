d500aaa380c7dc4c0c37384e7da109f10414adc2
Harshit yadav <harshityadav5499@gmail.com>
2025-10-13 18:39:56 +0530

    week 1 done

--- airdrop/getPda.ts
+++ airdrop/getPda.ts
@@ -4,11 +4,11 @@ const PROGRAM_ID = new PublicKey("TRBZyQHB3m68FGeVsqTK39Wm4xejadjVhP5MAZaKWDM");
 const USER = new PublicKey("9B9tXSndjpZxWazgibnfpuE5QoTjSx9sxtHRFBoHEyYm");
 
 (async () => {
-  const [pda, bump] = await PublicKey.findProgramAddress(
+  const [pda, bump] = await PublicKey.findProgramAddressSync(
     [Buffer.from("prereqs"), USER.toBuffer()],
     PROGRAM_ID
   );
-
+  console.log("user:", Buffer.from("prereqs"));
   console.log("PDA:", pda.toBase58());
   console.log("Bump:", bump);
 })();
