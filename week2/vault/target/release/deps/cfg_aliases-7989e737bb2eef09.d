dd5b8ee18432c3da1d7943731c75559e6a41e959
Harshit yadav <harshityadav5499@gmail.com>
2025-10-17 22:32:14 +0530

    vault -> init and deposite

---
+++ week2/vault/target/release/deps/cfg_aliases-7989e737bb2eef09.d
@@ -0,0 +1,7 @@
+/home/titan/Anchor_basics/vault/target/release/deps/libcfg_aliases-7989e737bb2eef09.rmeta: /home/titan/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg_aliases-0.2.1/src/lib.rs
+
+/home/titan/Anchor_basics/vault/target/release/deps/libcfg_aliases-7989e737bb2eef09.rlib: /home/titan/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg_aliases-0.2.1/src/lib.rs
+
+/home/titan/Anchor_basics/vault/target/release/deps/cfg_aliases-7989e737bb2eef09.d: /home/titan/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg_aliases-0.2.1/src/lib.rs
+
+/home/titan/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg_aliases-0.2.1/src/lib.rs:
---
+++ week2/vault/target/types/vault.ts
@@ -0,0 +1,145 @@
+/**
+ * Program IDL in camelCase format in order to be used in JS/TS.
+ *
+ * Note that this is only a type helper and is not the actual IDL. The original
+ * IDL can be found at `target/idl/vault.json`.
+ */
+export type Vault = {
+  "address": "6pyfw3NjSCATopSRmZ44wJHnBbMtnfcgRepkh34QJQr6",
+  "metadata": {
+    "name": "vault",
+    "version": "0.1.0",
+    "spec": "0.1.0",
+    "description": "Created with Anchor"
+  },
+  "instructions": [
+    {
+      "name": "deposit",
+      "discriminator": [
+        242,
+        35,
+        198,
+        137,
+        82,
+        225,
+        242,
+        182
+      ],
+      "accounts": [
+        {
+          "name": "vault",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  118,
+                  97,
+                  117,
+                  108,
+                  116
+                ]
+              },
+              {
+                "kind": "account",
+                "path": "signer"
+              }
+            ]
+          }
+        },
+        {
+          "name": "signer",
+          "writable": true,
+          "signer": true
+        },
+        {
+          "name": "systemProgram",
+          "address": "11111111111111111111111111111111"
+        }
+      ],
+      "args": [
+        {
+          "name": "amount",
+          "type": "u64"
+        }
+      ]
+    },
+    {
+      "name": "init",
+      "discriminator": [
+        220,
+        59,
+        207,
+        236,
+        108,
+        250,
+        47,
+        100
+      ],
+      "accounts": [
+        {
+          "name": "vault",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  118,
+                  97,
+                  117,
+                  108,
+                  116
+                ]
+              },
+              {
+                "kind": "account",
+                "path": "signer"
+              }
+            ]
+          }
+        },
+        {
+          "name": "signer",
+          "writable": true,
+          "signer": true
+        },
+        {
+          "name": "systemProgram",
+          "address": "11111111111111111111111111111111"
+        }
+      ],
+      "args": []
+    }
+  ],
+  "accounts": [
+    {
+      "name": "vault",
+      "discriminator": [
+        211,
+        8,
+        232,
+        43,
+        2,
+        152,
+        117,
+        119
+      ]
+    }
+  ],
+  "types": [
+    {
+      "name": "vault",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "account",
+            "type": "pubkey"
+          }
+        ]
+      }
+    }
+  ]
+};
